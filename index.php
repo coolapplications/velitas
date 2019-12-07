dbe8e5fed5e4876f44a7a473a05534ceb7d7cf36
Andres <amauricio.alvarez@udea.edu.co>
2019-12-07 05:31:49 -0500

    add query parameter

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <html>
  <head>
-  <title>Receta de la natilla</title>
+  <title><h1>Receta de la natilla<h1></title>
  </head>
  <body>
- <?php echo '<p>Leche y natilla maizena sabor a maracuyá</p>'; ?>
+ <?php
+ echo '¡Hola ' . htmlspecialchars($_GET["name"]) . '!';
+ echo '<p>Leche y natilla maizena sabor a maracuyá</p>'; ?>
  <img src="img/natilla.png">
- <img src="img/maizena.jpg">
+ <img src="img/maizena.jpeg">
  </body>
 </html>
