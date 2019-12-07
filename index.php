c2319800624977cb682cd9566bdb7ac2ad218f5f
Andres <amauricio.alvarez@udea.edu.co>
2019-12-07 05:15:16 -0500

    images addition

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
   <title>Receta de la natilla</title>
  </head>
  <body>
- <?php echo '<p>Leche y natilla maicena sabor a maracuyá</p>'; ?>
+ <?php echo '<p>Leche y natilla maizena sabor a maracuyá</p>'; ?>
+ <img src="img/natilla.png">
+ <img src="img/maizena.jpg">
  </body>
 </html>
